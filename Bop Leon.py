d4e9bb8993c2dfb34c1b8fc5a9c1b59eb3169e3d
llacatusurectory <leon.lacatusu@rectorywolves.org>
2024-04-05 09:56:34 -0900

    Fixed the arrows of the game

--- Bop Leon.py
+++ Bop Leon.py
@@ -6,80 +6,63 @@ print ("Hi mrs Karp")
 sense = SenseHat()
 a = (255,255,255)
 b = (0,0,0)
-arrows = [[
-a,a,a,b,b,a,a,a,
-a,a,b,b,b,b,a,a,
-a,b,b,b,b,b,b,a,
-a,a,a,b,b,a,a,a,
-a,a,a,b,b,a,a,a,
-a,a,a,b,b,a,a,a,
-a,a,a,b,b,a,a,a,
-a,a,a,b,b,a,a,a,
-],[
-a,a,a,b,b,a,a,a,
-a,a,a,b,b,a,a,a,
-a,a,a,b,b,a,a,a,
-a,a,a,b,b,a,a,a,
-a,a,a,b,b,a,a,a,
-a,b,b,b,b,b,b,a,
-a,a,b,b,b,b,a,a,
-a,a,a,b,b,a,a,a,
-],[
-a,a,a,a,a,a,a,a,
-a,a,a,a,a,b,a,a,
-a,a,a,a,a,b,b,a,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-a,a,a,a,a,b,b,a,
-a,a,a,a,a,b,a,a,
-a,a,a,a,a,a,a,a,
-],[
-a,a,a,a,a,a,a,a,
-a,a,b,a,a,a,a,a,
-a,b,b,a,a,a,a,a,
-b,b,b,b,b,b,b,b,
-b,b,b,b,b,b,b,b,
-a,b,b,a,a,a,a,a,
-a,a,b,a,a,a,a,a,
-a,a,a,a,a,a,a,a,
-],[
-a,a,b,b,b,b,a,a,
-a,b,b,b,b,b,b,a,
-b,b,b,b,b,b,b,b,
-b,b,b,a,a,b,b,b,
-b,b,b,a,a,b,b,b,
-b,b,b,b,b,b,b,b,
-a,b,b,b,b,b,b,a,
-a,a,b,b,b,b,a,a,]]
+arrows = ['''
 
-count = ['up','down','right','left','middle']
-x = rn.randint(0,4)
-sense.set_pixels(arrows[x])
+^
+|
+
+''','''
+
+|
+v
+
+''', '''
+
+->
+
+''','''
+
+<-
+
+''']
+
+count = ['up','down','right','left']
+x = rn.randint(0,3)
+print(arrows[x])
 game = 1
 score = 0
 while(game == 1):
+	sense.set_pixels([
+a,a,a,b,b,a,a,a,
+a,a,b,b,b,b,a,a,
+a,b,b,b,b,b,b,a,
+a,a,a,b,b,a,a,a,
+a,a,a,b,b,a,a,a,
+a,a,a,b,b,a,a,a,
+a,a,a,b,b,a,a,a,
+a,a,a,b,b,a,a,a,
+])
+	limit = 5
+	s = time.time()
+	event = sense.stick.wait_for_event()
+	e = time.time()
+	t = (e-s)
 
-limit = 5
-s = time.time()
-event = sense.stick.wait_for_event()
-e = time.time()
-t = (e-s)
 
+	if t<limit and event.action == "pressed" and event.direction == count[x]:
+		x = rn.randint(0,3)
+		print(arrows[x])
+		score = score + 1
+	elif event.action == "pressed" and t>limit:
+		game = 0
+		print("Game over. You took too long.")
+	elif event.action == "pressed" and event.direction != count[x]:
+		sense.clear(255,0,0)
+		game = 0
+		print("Game over. Wrong direction. No rizz.")
 
-if t<limit and event.action == "pressed" and event.direction == count[x]:
-x = rn.randint(0,4)
-sense.set_pixels(arrows[x])
-score = score + 1
-elif event.action == "pressed" and t>limit:
-game = 0
-print("Game over. Time's up.")
-elif event.action == "pressed" and event.direction != count[x]:
-sense.clear(255,0,0)
-game = 0
-print("Game over. Wrong direction. No rizz.")
-
-if game != 1:
-print("Your score was..." + str(score) + " points.")
+	if game != 1:
+		print("Your score was..." + str(score) + " points.")
 
 
 
